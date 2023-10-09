875e31e2cf755809834de1a5b1f4ce00f47b3a0f
xiangyuT <xiangyu.tian@intel.com>
2023-10-09 10:00:13 +0800

    refine

--- openai_api_server.py
+++ openai_api_server.py
@@ -26,12 +26,6 @@ import time
 
 app = FastAPI()
 openai_cache: Optional[Cache] = None
-cache_dir = ""
-cache_file_key = ""
-
-class CacheData(BaseModel):
-    prompt: str
-    answer: Optional[str] = ""
 
 embedding_onnx = EmbeddingOnnx()
 class WrapEvaluation(SearchDistanceEvaluation):
@@ -82,30 +76,6 @@ async def get_cache(cache_data: CacheData) -> CacheData:
 async def get_cache() -> str:
     cache.flush()
     return "successfully flush the cache"
-
-
-@app.get("/cache_file")
-async def get_cache_file(key: str = "") -> FileResponse:
-    global cache_dir
-    global cache_file_key
-    if cache_dir == "":
-        raise HTTPException(
-            status_code=403,
-            detail="the cache_dir was not specified when the service was initialized",
-        )
-    if cache_file_key == "":
-        raise HTTPException(
-            status_code=403,
-            detail="the cache file can't be downloaded because the cache-file-key was not specified",
-        )
-    if cache_file_key != key:
-        raise HTTPException(status_code=403, detail="the cache file key is wrong")
-    zip_filename = cache_dir + ".zip"
-    with zipfile.ZipFile(zip_filename, "w", compression=zipfile.ZIP_DEFLATED) as zipf:
-        for root, _, files in os.walk(cache_dir):
-            for file in files:
-                zipf.write(os.path.join(root, file))
-    return FileResponse(zip_filename)
 
 @app.api_route(
     "/v1/chat/completions",
@@ -178,13 +148,6 @@ def main():
     )
     parser.add_argument(
         "-p", "--port", type=int, default=8000, help="the port to listen on"
-    )
-    parser.add_argument(
-        "-d", "--cache-dir", default="gptcache_data", help="the cache data dir"
-    )
-    parser.add_argument("-k", "--cache-file-key", default="", help="the cache file key")
-    parser.add_argument(
-        "-f", "--cache-config-file", default=None, help="the cache config file"
     )
 
     args = parser.parse_args()
